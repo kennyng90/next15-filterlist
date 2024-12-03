9c42970230d24cddd934d6185849529e8d948f4b
Aurora Scharff <aurora.sofie@gmail.com>
2024-12-03 16:13:49 +0100

    Upgrade to eslint 9

--- app/[tab]/not-found.tsx
+++ app/[tab]/not-found.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function NotFound() {
-  return <div className="bg-black p-4 text-center text-white">Invalid tab</div>;
+  return <div className="bg-black p-4 text-center text-white dark:bg-white dark:text-black">Invalid tab</div>;
 }
